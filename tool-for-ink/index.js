8daa949015203b625abe273fc712b3d00e390fff
kay404 <kay20475@hotmail.com>
2023-06-17 17:43:52 +0800

    support multi contract

--- tool-for-ink/index.js
+++ tool-for-ink/index.js
@@ -4,8 +4,13 @@ const utils = require('./utils');
 const eccrypto = require('eccrypto');
 const keccak256 = require('keccak256');
 const secp256k1 = require('secp256k1');
-const { ApiPromise, HttpProvider, Keyring, WsProvider } = require('@polkadot/api');
-const { BN, BN_ONE } = require("@polkadot/util");
+const {
+  ApiPromise,
+  HttpProvider,
+  Keyring,
+  WsProvider,
+} = require('@polkadot/api');
+const { BN, BN_ONE } = require('@polkadot/util');
 const { ContractPromise } = require('@polkadot/api-contract');
 const request = require('request');
 const { u8, u128, Struct, Vector, Bytes } = require('scale-ts');
@@ -33,7 +38,6 @@ let omniverseContract;
 // Private key
 let secret = JSON.parse(fs.readFileSync('./.secret').toString());
 let testAccountPrivateKey = secret.sks[secret.index];
-let mpcPublicKey = secret.mpc;
 let privateKeyBuffer = Buffer.from(utils.toByteArray(testAccountPrivateKey));
 let publicKeyBuffer = eccrypto.getPublic(privateKeyBuffer);
 let publicKey = '0x' + publicKeyBuffer.toString('hex').slice(2);
@@ -43,15 +47,19 @@ let sender = keyring.addFromSeed(privateKeyBuffer);
 async function init(chainName, tokenId) {
   netConfig = config.get(chainName);
   if (!netConfig) {
-      console.log('Config of chain (' + chainName + ') not exists');
-      return false;
+    console.log('Config of chain (' + chainName + ') not exists');
+    return false;
   }
   // Construct
   const provider = new WsProvider(netConfig.nodeAddress);
   api = await ApiPromise.create({ provider, noInitWarn: true });
   chainId = netConfig.omniverseChainId;
   let metadata = JSON.parse(fs.readFileSync(netConfig.metadataPath));
-  omniverseContract = new ContractPromise(api, metadata, netConfig.omniverseContractAddress[tokenId]);
+  omniverseContract = new ContractPromise(
+    api,
+    metadata,
+    netConfig.omniverseContractAddress[tokenId]
+  );
   return true;
 }
 
@@ -77,10 +85,16 @@ let getRawData = (txData) => {
   console.log(bData);
 
   let fungible = Fungible.dec(txData.payload);
-  console.log('Buffer.from([fungible.op])', fungible, Buffer.from([fungible.op]), Buffer.from(fungible.ex_data), Buffer.from(
-    new BN(fungible.amount).toString('hex').padStart(32, '0'),
-    'hex'
-  ))
+  console.log(
+    'Buffer.from([fungible.op])',
+    fungible,
+    Buffer.from([fungible.op]),
+    Buffer.from(fungible.ex_data),
+    Buffer.from(
+      new BN(fungible.amount).toString('hex').padStart(32, '0'),
+      'hex'
+    )
+  );
   bData = Buffer.concat([bData, Buffer.from([fungible.op])]);
 
   bData = Buffer.concat([bData, Buffer.from(fungible.ex_data)]);
@@ -97,30 +111,44 @@ let getRawData = (txData) => {
 
 async function initialize(members) {
   let ret = await ink.sendTransaction(
-    omniverseContract, 'omniverse::setCoolingDown', sender, [netConfig.coolingDown]);
+    omniverseContract,
+    'omniverse::setCoolingDown',
+    sender,
+    [netConfig.coolingDown]
+  );
   if (!ret) {
     // Error
   }
   ret = await ink.sendTransaction(
-    omniverseContract, 'fungibleToken::setMembers', sender, [members]);
+    omniverseContract,
+    'fungibleToken::setMembers',
+    sender,
+    [members]
+  );
   if (!ret) {
     // Error
   }
 }
 
 async function sendTransaction(to, amount, op) {
-  let nonce = await ink.contractCall(omniverseContract, "omniverse::getTransactionCount", sender.address, [publicKey]);
-  console.log('nonce', nonce)
+  let nonce = await ink.contractCall(
+    omniverseContract,
+    'omniverse::getTransactionCount',
+    sender.address,
+    [publicKey]
+  );
+  console.log('nonce', nonce);
   let payload = Fungible.enc({
     op: op,
     ex_data: Array.from(Buffer.from(to.slice(2), 'hex')),
     amount: BigInt(amount),
   });
-  
+
   let txData = {
     nonce: parseInt(nonce.toString()),
     chainId: chainId,
-    initiateSc: '0x' + Buffer.from(netConfig.omniverseContractAddress).toString('hex'),
+    initiateSc:
+      '0x' + Buffer.from(omniverseContract.address.toJSON()).toString('hex'),
     from: publicKey,
     payload: utils.toHexString(payload),
   };
@@ -137,7 +165,11 @@ async function sendTransaction(to, amount, op) {
   // test end
   console.log(txData);
   let ret = await ink.sendTransaction(
-    omniverseContract, 'fungibleToken::sendOmniverseTransaction', sender, [txData]);
+    omniverseContract,
+    'fungibleToken::sendOmniverseTransaction',
+    sender,
+    [txData]
+  );
   if (!ret) {
     // Error
   }
@@ -146,7 +178,12 @@ async function sendTransaction(to, amount, op) {
 }
 
 async function omniverseBalanceOf(pk) {
-  let amount = await ink.contractCall(omniverseContract, "fungibleToken::balanceOf", sender.address, [pk]);
+  let amount = await ink.contractCall(
+    omniverseContract,
+    'fungibleToken::balanceOf',
+    sender.address,
+    [pk]
+  );
   return amount;
 }
 
@@ -202,10 +239,10 @@ async function accountInfo() {
       'Switch the index of private key to be used'
     )
     .option('-a, --account', 'Show the account information')
-    .option('-ti, --tokenId', 'The omniverse token id')
+    .option('-ti, --tokenId <tokenId>', 'The omniverse token id')
     .parse(process.argv);
-    
-  let tokenId = program.opts().account;
+
+  let tokenId = program.opts().tokenId;
   if (program.opts().account) {
     await accountInfo();
   } else if (program.opts().initialize) {
@@ -217,7 +254,7 @@ async function accountInfo() {
       );
       return;
     }
-    
+
     if (!(await init(program.opts().initialize[0], tokenId))) {
       return;
     }
@@ -225,16 +262,14 @@ async function accountInfo() {
     let members = [];
     let param = program.opts().initialize.slice(1);
     for (let i = 0; i < param.length; i++) {
-        let m = param[i].split('|');
-        members.push({
-          chain_id: m[0],
-          contract_address: m[1]
-        });
+      let m = param[i].split('|');
+      members.push({
+        chain_id: m[0],
+        contract_address: m[1],
+      });
     }
 
-    await initialize(
-      members
-    );
+    await initialize(members);
     api.disconnect();
   } else if (program.opts().transfer) {
     if (program.opts().transfer.length != 3) {
@@ -266,11 +301,7 @@ async function accountInfo() {
     if (!(await init(program.opts().mint[0], tokenId))) {
       return;
     }
-    await sendTransaction(
-      program.opts().mint[1],
-      program.opts().mint[2],
-      MINT
-    );
+    await sendTransaction(program.opts().mint[1], program.opts().mint[2], MINT);
   } else if (program.opts().burn) {
     if (program.opts().burn.length != 3) {
       console.log(
@@ -284,11 +315,7 @@ async function accountInfo() {
     if (!(await init(program.opts().mint[0], tokenId))) {
       return;
     }
-    await sendTransaction(
-      program.opts().burn[1],
-      program.opts().burn[2],
-      BURN
-    );
+    await sendTransaction(program.opts().burn[1], program.opts().burn[2], BURN);
   } else if (program.opts().omniBalance) {
     if (program.opts().omniBalance.length > 2) {
       console.log(
@@ -308,9 +335,7 @@ async function accountInfo() {
     if (!(await init(program.opts().omniBalance[0], tokenId))) {
       return;
     }
-    let amount = await omniverseBalanceOf(
-      account
-    );
+    let amount = await omniverseBalanceOf(account);
     console.log('amount', amount.toString());
     api.disconnect();
   } else if (program.opts().switch) {
